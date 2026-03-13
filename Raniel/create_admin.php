2664a8701991700b4145e1da59184dc318a258ae
mutas2025 <mutas@csr-scc.edu.ph>
2026-03-13 19:04:11 +0800

    ds
    fg

--- Raniel/create_admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Run this file once to generate the hash
- $password = "1234"; 
-echo password_hash($password, PASSWORD_BCRYPT);
-?>
--- kenn/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirect to the login page
-header("Location: login.php");
-exit; // Ensure that no other code is executed
-?>
